9961f79b16fa44763e64ed4f89c01add3e57aa80
Daniel Castilho <dan.castilho@gmail.com>
2021-06-02 17:22:33 -0400

    Create Component CollectionPreview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import SHOP_DATA from "./shop.data";
 
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
 class ShopPage extends Component {
 	constructor(props) {
 		super(props);
@@ -12,8 +14,15 @@ class ShopPage extends Component {
 	}
 
     render() {
+        const { collections } = this.state;
         return (
-            <div>SHOP PAGE</div>
+            <div className="shop-page">
+                {
+                    collections.map(({id, ...otherCollectionProps }) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
+            </div>
         )
     }
 }
