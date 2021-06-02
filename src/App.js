2bd9dc0493bc5a653d2e91c7e2606260a03fefa7
Daniel Castilho <dan.castilho@gmail.com>
2021-06-02 15:47:04 -0400

    Configure withRouter()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
 const HatsPage = () => (
 	<div>
-		<h1>HATS PAGE</h1>
+		<h1>HATS PAGE </h1>
 	</div>
 );
 
@@ -14,8 +15,8 @@ function App() {
 	return (
 		<div>
 			<Switch>
-				<Route path="/hats" component={HatsPage} />
 				<Route exact path="/" component={HomePage} />
+				<Route path="/hats" component={HatsPage} />
 			</Switch>
 		</div>
 	);
