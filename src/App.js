0111edd8adb0ea489675c73f8da4883589f95b3e
Daniel Castilho <dan.castilho@gmail.com>
2021-06-03 19:51:48 -0400

    Google Sign In Authentication #02

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
@@ -7,18 +7,37 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-	return (
-		<div>
-			<Header />
-			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route path="/shop" component={ShopPage} />
-				<Route path="/signin" component={SignInAndSignUpPage} />
-			</Switch>
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			currentUser: null,
+		};
+	}
+
+	componentDidMount() {
+		auth.onAuthStateChanged(user => {
+			this.setState({ currentUser: user });
+
+			console.log(user);
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/shop" component={ShopPage} />
+					<Route path="/signin" component={SignInAndSignUpPage} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
