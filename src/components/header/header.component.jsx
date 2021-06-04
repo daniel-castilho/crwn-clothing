074427211a87a4e8f85969078ce231123cde5141
Daniel Castilho <dan.castilho@gmail.com>
2021-06-04 00:45:21 -0400

    User Redirect and User Action Type

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,30 +8,33 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => {
-	return (
-		<div className="header">
-			<Link className="logo-container" to="/">
-				<Logo className="logo" />
+const Header = ({ currentUser }) => (
+	<div className="header">
+		<Link className="logo-container" to="/">
+			<Logo className="logo" />
+		</Link>
+		<div className="options">
+			<Link className="option" to="/shop">
+				SHOP
 			</Link>
-			<div className="options">
-				<Link className="option" to="/shop">
-					SHOP
+			<Link className="option" to="/shop">
+				CONTACT
+			</Link>
+			{currentUser ? (
+				<div className="option" onClick={() => auth.signOut()}>
+					SIGN OUT
+				</div>
+			) : (
+				<Link className="option" to="/signin">
+					SIGN IN
 				</Link>
-				<Link className="option" to="/shop">
-					CONTACT
-				</Link>
-				{
-					currentUser ?
-					<div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-					:
-					<Link className="option" to="/signin">SIGN IN</Link>
-				}
-			</div>
+			)}
 		</div>
-	);
-};
+	</div>
+);
 
-const mapStateToProps = state => ({ currentUser: state.user.currentUser });
+const mapStateToProps = (state) => ({
+	currentUser: state.user.currentUser,
+});
 
 export default connect(mapStateToProps)(Header);
