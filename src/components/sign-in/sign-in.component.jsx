3e4dcae7f31224cb3a40cdcf9f4088433f6fb03f
Daniel Castilho <dan.castilho@gmail.com>
2021-06-03 20:08:22 -0400

    Integrated Firebase into Application, created sign in page with sign in Google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -52,11 +52,12 @@ class SignIn extends Component {
 						handleChange={this.handleChange}
 						required
 					/>
-
-					<CustomButton type="submit"> sign in </CustomButton>
-					<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-						Sign in with Google
-					</CustomButton>
+					<div className="buttons">
+						<CustomButton type="submit"> sign in </CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+							Sign in with Google
+						</CustomButton>
+					</div>
 				</form>
 			</div>
 		);
