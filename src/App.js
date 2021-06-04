bdd664cf251864bcb9a37a3313d93d8ea70da903
Daniel Castilho <dan.castilho@gmail.com>
2021-06-04 00:23:48 -0400

    Added Redux, and implemented userReducer and userActions and app components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-
+import { connect } from "react-redux";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
@@ -8,33 +8,27 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import setCurrentUser from "./redux/user/user.actions";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			currentUser: null,
-		};
-	}
-
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
+		const { setCurrentUser } = this.props;
+
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
 			if (userAuth) {
 				const userRef = await createUserProfileDocument(userAuth);
 
 				userRef.onSnapshot((snapShop) => {
-					this.setState({
-						currentUser: {
-							id: snapShop.id,
-							...snapShop.data(),
-						},
+					setCurrentUser({
+						id: snapShop.id,
+						...snapShop.data(),
 					});
 				});
-				this.setState({ currentUser: userAuth });
 			}
+
+			setCurrentUser({ userAuth });
 		});
 	}
 
@@ -56,4 +50,8 @@ class App extends Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+	setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
