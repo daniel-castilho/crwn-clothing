ea5ed4bcb422025e3fe5c95633657df9e85ee217
Daniel Castilho <dan.castilho@gmail.com>
2021-06-06 18:18:46 -0400

    Remove Items from Cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,3 +1,5 @@
+import { bindActionCreators } from "redux";
+
 export const addItemToCart = (cartItems, cartItemToAdd) => {
 	const existingCartItem = cartItems.find(
 		(cartItem) => cartItem.id === cartItemToAdd.id
@@ -12,4 +14,8 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 	}
 
 	return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const clearItemsFromCart = (cartItems, cartItemToRemove) => {
+	return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
 };
