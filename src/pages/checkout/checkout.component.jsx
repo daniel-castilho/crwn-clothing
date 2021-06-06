df450247a997752b34c23b0a05bd0324b46e72c4
Daniel Castilho <dan.castilho@gmail.com>
2021-06-06 13:47:34 -0400

    Checkout Page

---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./checkout.styles.scss"
+
+const CheckoutPage = () => {
+    return (
+        <div>
+            Checkout PAGE
+        </div>
+    )
+}
+
+export default CheckoutPage
